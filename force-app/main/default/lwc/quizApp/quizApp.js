8096ef07f7421667e316f9d2a2ccdbc860ce056c
Jatin Pal <jatinpal556@gmail.com>
2024-03-30 05:25:15 +0000

    commit 2

--- force-app/main/default/lwc/quizApp/quizApp.js
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -1,3 +1,55 @@
 import { LightningElement } from 'lwc';
 
-export default class QuizApp extends LightningElement {}
+export default class QuizApp extends LightningElement {
+
+    selected = {};
+    questions = [
+        {
+            id: "Question 1",
+            question: "Which of the following is not directive?",
+            options: {
+                a: "for:each",
+                b: "iterator",
+                c: "map loop"
+            },
+            answer: "c"
+        },
+        {
+            id: "Question 2",
+            question: "Which of the following is not a template loop?",
+            options: {
+                a: "for:each",
+                b: "if:true",
+                c: "@track"
+            },
+            answer: "c"
+        },
+        {
+            id: "Question 1",
+            question: "Which of the following is invalid in LWC folder??",
+            options: {
+                a: "svg",
+                b: "js",
+                c: "apex"
+            },
+            answer: "c"
+        }
+    ]
+
+    changeHandler = (event) => {
+        const {name, value} = event.target;
+        this.selected = {...this.selected, [name]: value};
+    }
+
+    get allNotSelected() {
+        return !(Object.keys(this.selected).length === this.questions.length)
+    }
+
+    submitHandler = () => {
+
+    }
+
+    resetHandler = () => {
+
+    }
+}
